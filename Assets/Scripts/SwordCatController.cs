cfde76eac3f172963b9915a044fe3109b0ac45e5
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-10-01 15:48:59 +1000

    Initial commit

---
+++ Assets/Scripts/SwordCatController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCatController : MonoBehaviour 
+{
+    // Class means defining a new component, in this case SwordCatController
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position; //declared a var of type Vector2 called position
+        position.x = position.x + 0.1f;//floating point, float type
+        transform.position = position;
+    }
+}
