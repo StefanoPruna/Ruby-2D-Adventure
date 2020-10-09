533aa08ee798a0fc3e67f6145f2630d673ceaf0d
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-10-09 16:04:50 +1100

    Map
    Fixed the tile map;
    Added colliders, tilemap collider;
    Added Health script;
    Improved the speed line in the code

--- Assets/Scripts/SwordCatController.cs
+++ Assets/Scripts/SwordCatController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SwordCatController : MonoBehaviour 
 {
+    public int maxHealth;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal, vertical;
+
+    public float mySpeed;
 
     // Class means defining a new component, in this case SwordCatController
     void Start()
@@ -17,6 +22,8 @@ public class SwordCatController : MonoBehaviour
         Application.targetFrameRate = 10;*/
 
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -40,8 +47,21 @@ public class SwordCatController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        /*I can specify the speed here
+         * position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        or i can create the public var, assign the speed in the inspector/Unity and then use that var in the line of code as per below*/
+        position.x = position.x + mySpeed * horizontal * Time.deltaTime;
+        position.y = position.y + mySpeed * vertical * Time.deltaTime;
+
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)//void means what value the function will return, in this case nothing, because
+                                 //it just changes the Health
+    {
+        //Matchf.Clamp is a built-in function in which the var will not go below 0 and above the maxHealth
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
